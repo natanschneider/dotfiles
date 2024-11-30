da9dcb4d55698f1ca81f9945ed7d2f453a029fba
Natan Schneider <natanschneider@protonmail.com>
2024-11-30 11:33:53 -0300

    *feat* add linting on save

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -35,7 +35,25 @@ require("lazy").setup({
       },
       config = true,
     },
-
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = { eslint = {} },
+        setup = {
+          eslint = function()
+            require("lazyvim.util").lsp.on_attach(function(client)
+              if client.name == "eslint" then
+                client.server_capabilities.documentFormattingProvider = true
+              elseif client.name == "tsserver" then
+                client.server_capabilities.documentFormattingProvider = false
+              end
+            end)
+          end,
+        },
+      },
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
