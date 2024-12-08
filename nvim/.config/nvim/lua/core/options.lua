4156deba2ebc97c9e18de720c66a955bc5a7e2a7
Natan Schneider <natanschneider@protonmail.com>
2024-12-07 21:22:44 -0300

    *feat* add vim behavior options

---
+++ nvim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,14 @@
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.mouse = 'a'
+vim.o.autoindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.tabstop = 3
+vim.o.scrolloff = 4
+vim.o.smartindent = true
+vim.o.fileencoding = 'utf-8'
+vim.o.autoindent = true
+
