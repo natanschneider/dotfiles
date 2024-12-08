2773fbf7c3f31facf9a5abd34f5aa7b538f65be5
Natan Schneider <natanschneider@protonmail.com>
2024-12-07 21:54:23 -0300

    *fix* indentation options

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -6,9 +6,13 @@ vim.o.autoindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.tabstop = 3
 vim.o.scrolloff = 4
 vim.o.smartindent = true
 vim.o.fileencoding = 'utf-8'
 vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.breakindent = true
 
