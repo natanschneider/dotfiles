d28c44e67f1e8509d298b5565462f93710a6581b
Natan Moura Schneider <natan.schneider@deltasul.com.br>
2025-02-06 17:05:58 -0300

    *feat* add shortcut for formatting

--- nvim/.config/nvim/lua/user/plugins/conform.lua
+++ nvim/.config/nvim/lua/user/plugins/conform.lua
@@ -1,5 +1,15 @@
 return {
   'stevearc/conform.nvim',
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
   opts = {
     formatters_by_ft = {
       php = { "pint" },
