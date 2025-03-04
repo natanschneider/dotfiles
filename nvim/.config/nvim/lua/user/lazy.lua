070eb5d4f7a9287911727836fdb667c80867e05b
natanschneider <natanschneider@protonmail.com>
2025-03-04 18:30:38 -0300

    *feat* set nvim theme to catppuccin-mocha

--- nvim/.config/nvim/lua/user/lazy.lua
+++ nvim/.config/nvim/lua/user/lazy.lua
@@ -15,6 +15,6 @@ require("lazy").setup('user.plugins', {
     notify = false,
   },
   install = {
-    colorscheme = { "tokyonight", "habamax" },
+    colorscheme = { "catppuccin-mocha" },
   },
 })
