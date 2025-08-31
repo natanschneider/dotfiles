6fd1eeeaace7d22d840f2244102ce16c66fbd8e5
Natãn Schneider <natanschneider@protonmail.com>
2025-08-31 13:16:08 -0300

    *feat* add linting and formating on file save

--- nvim/.config/nvim/lua/user/misc.lua
+++ nvim/.config/nvim/lua/user/misc.lua
@@ -12,3 +12,16 @@ vim.api.nvim_create_autocmd('textyankpost', {
     vim.highlight.on_yank({ timeout = 100 })
   end,
 })
+
+vim.api.nvim_create_augroup("LanguageSpecificCommands", { clear = true })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.js",
+  command = "!npm run lint:fix | npm run format",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.php",
+  command = "!composer run lint | composer run refactor",
+})
+
